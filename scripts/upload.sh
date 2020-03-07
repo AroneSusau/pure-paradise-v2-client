9dad111ef1bf4cd2b3a61ca7294f2fc50341f627
Arone Susau <arone.s@live.com.au>
2020-03-07 21:11:39 +1100

    updated upload

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,8 +5,9 @@ npm run build production
 
 # Upload distribution to s3
 aws s3 cp dist/bundle.js s3://pure-paradise-v2
+aws s3 cp dist/1.bundle.js s3://pure-paradise-v2
 aws s3 cp dist/favicon.ico s3://pure-paradise-v2
-aws s3 cp dist/index.css s3://pure-paradise-v2
+aws s3 cp dist/styles.css s3://pure-paradise-v2
 aws s3 cp dist/index.html s3://pure-paradise-v2
 aws s3 cp dist/title.png s3://pure-paradise-v2
 
