02808261d2b416afc9327f704511b4a3b2e96f91
Arone Susau <arone.s@live.com.au>
2021-03-05 17:46:42 +1100

    fixing prod url

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,7 +3,10 @@ import io from "socket.io-client";
 import types from "./../enums/types.js";
 import store from "./../store/index.js";
 
-const socket = io("http://localhost:3000");
+const prod = "https://pure-paradise-v2.herokuapp.com";
+const dev = "http://localhost:3000";
+
+const socket = io(process.env.NODE_ENV === "production" ? prod : dev);
 
 socket.on("client:command", response => {
   if (response.general) {
